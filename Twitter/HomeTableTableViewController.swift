d1f3c66620f62899f20d035f2d3dc42275fc17bd
ChristianPinto-hub <72939362+ChristianPinto-hub@users.noreply.github.com>
2021-10-10 15:23:34 -0400

    Implements pull to refresh

--- Twitter/HomeTableTableViewController.swift
+++ Twitter/HomeTableTableViewController.swift
@@ -13,12 +13,16 @@ class HomeTableTableViewController: UITableViewController {
     var tweetArray = [NSDictionary]()
     var numberOfTweets: Int!
     
+    let myRefreshControl = UIRefreshControl()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         loadTweets()
+        myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
+        tableView.refreshControl = myRefreshControl
     }
     
-    func loadTweets() {
+    @objc func loadTweets() {
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
         let myParams = ["count": 10]
         TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success:
@@ -28,7 +32,10 @@ class HomeTableTableViewController: UITableViewController {
             for tweet in tweets {
                 self.tweetArray.append(tweet)
             }
+                
             self.tableView.reloadData()
+            self.myRefreshControl.endRefreshing()
+                
         }, failure: { (Error) in
             print("Could not retrieve tweets! Oh no!")
         })
